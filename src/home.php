76b325156fc28dd3fa9a96acfd48560ea6eb7516
jetscholar <mrz@jetscholar.com>
2022-03-17 23:43:37 +1100

    home page update

--- src/home.php
+++ src/home.php
@@ -4,16 +4,37 @@ session_start();
 if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
  ?>
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html>
-<head>
-	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-</head>
-<body>
-     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
-     <a href="logout.php">Logout</a>
-</body>
+    <head>
+        <title>Welcome to the Nucular Power Plant</title>
+        <link rel = "stylesheet" href = "assets/css/main.css">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <!-- Side navigation -->
+        <div class="sidenav">
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Clients</a>
+            <a href="#">Contact</a>
+            <a href="logout.php">Logout</a>
+        </div>
+
+
+        <!--This is where the Main Content of the Page Goes-->
+        <div class="main">
+        <h1></h1>
+     
+             <h2>Hello <?php echo $_SESSION['user_name']; ?>, Welcome to the ACME Nuclear Power Plant</h2>
+                <div>
+                </div>
+                <img src="assets/images/NucularPowerPlantHomePageImage.jpg">
+                <div>
+                </div>
+        </div>
+    </body>
 </html>
 
 <?php 
@@ -21,4 +42,4 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
      header("Location: index.php");
      exit();
 }
- ?>
+?>
